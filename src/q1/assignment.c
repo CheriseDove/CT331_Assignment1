0f12d2dc70d1cf49d6598e22f68627e5ba263725
Rachel Coleman <43268365+CheriseDove@users.noreply.github.com>
2018-10-24 19:22:57 +0100

    Update As1 Q1
    Code cleaner

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
 
-int main(int arg, char* argc[]){
-  printf("Hello assignment1.\n");
-  return 0;
+//int main(int arg, char* argc[]) {
+int main(){
+	int  integer;
+	int *integerPointer;
+	long int longg;
+	double *dp;
+	char **R ;
+
+	printf("The size of an Integer is %lu bytes.\n", sizeof(int));
+	printf("The size of an Integer Pointer is %d bytes.\n", sizeof(integerPointer));
+	printf("The size of a Long is %lu bytes.\n", sizeof(long));
+	printf("The size of a Double Pointer is %lu bytes.\n", sizeof(double));
+	printf("The size of a Character Pointer is %d bytes.\n" ,sizeof(R));
 }
