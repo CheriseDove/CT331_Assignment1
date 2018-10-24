c27255c39a187123762c396319725e03ff59814f
Rachel Coleman <43268365+CheriseDove@users.noreply.github.com>
2018-10-24 18:30:39 +0100

    Updated Assignment 1, Question 1
    works

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
 
-int main(int arg, char* argc[]){
-  printf("Hello assignment1.\n");
-  return 0;
+int main(int arg, char* argc[]) {
+//int main(){
+	int  integer = 9;
+
+	int i = 18;
+	int *integerPointer;
+	integerPointer = &i;
+
+	long int longg = 27;
+
+	double j = 36.5;
+	double *dp;
+	dp = &j;
+
+	char** R;
+
+	printf("The size of an Integer is %x\n", integer);
+	printf("The size of an Integer Pointer is %x\n", sizeof(integerPointer));
+	printf("The size of a Long is %x\n", sizeof(longg));
+	printf("The size of a Double Pointer is %x\n", sizeof(dp));
+	printf("The size of a Character is %x\n", sizeof(R));
 }
