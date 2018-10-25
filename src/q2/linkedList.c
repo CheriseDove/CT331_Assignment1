e3b003af059f121019a44a7fe97bead03dd04409
Rachel Coleman <43268365+CheriseDove@users.noreply.github.com>
2018-10-25 21:42:08 +0100

    Update linkedList.c

--- src/q2/linkedList.c
+++ src/q2/linkedList.c
@@ -9,6 +9,19 @@ typedef struct listElementStruct{
   struct listElementStruct* next;
 } listElement;
 
+typedef struct nodePtr
+{
+	struct nodePtr *next;
+	char* data;
+	listElement** list;
+}nodePtr;
+
+typedef struct node
+{
+	struct node *next;
+	char* data;
+	listElement** list;
+}node;
 //Creates a new linked list element with given content of size
 //Returns a pointer to the element
 listElement* createEl(char* data, size_t size){
@@ -58,4 +71,124 @@ void deleteAfter(listElement* after){
   //need to free the memory because we used malloc
   free(delete->data);
   free(delete);
+}
+
+//Returns the number of elements in a linked list
+int length(listElement* list) {
+	listElement* current = list;
+	int count = 0;
+	while (current != NULL) {
+		count++;
+		current = current->next;
+	}
+	return count;
+}
+
+//Push a new element. Update List Ref.
+void push(listElement** list, char* data, size_t size) {
+	/* allocate node */
+
+	listElement * new_node;
+	new_node = malloc(sizeof(listElement));
+	/* put in the data */
+	new_node->data = data;
+	/* link the old list off the new node */
+	new_node->next = (*list);
+
+	/* move the head to point to the new node */
+	(*list) = new_node;
+
+}
+
+//Populate element from the head. Update List Ref.
+listElement* pop(listElement** *list) {
+	node* prev;
+	node* iter;
+	listElement* popb;
+	if (!list) return NULL;
+	if (!list-> front) return NULL;
+	prev = NULL;
+	iter = list->front;
+	while (iter != list->rear)
+	{
+		prev = iter; //not the rear, there must be a node after this
+		iter = iter->next; //not null, could be rear
+	}
+	if (prev == NULL) { list->front = NULL; }
+	else prev->next = NULL;
+	printf("%f\n", list->rear->d->val);
+	popb = list->tail->d;
+	free(list->tail); //free old rear
+	list->rear = prev;  //new rear
+	return popb;
+}
+
+//Enqueue a new element onto the head. Update list Ref.
+void enqueue(listElement** list, char* data, size_t size) {
+	node* temp;
+	node* front;
+	node* rear;
+
+	if (temp == NULL)
+	{
+		return 0;
+	}
+	else if (front == NULL && rear == NULL)
+	{
+		strcpy(temp->next, data);
+		rear = temp;
+		rear->next = NULL;
+		front = rear;
+		temp = NULL;
+	}
+	else
+	{
+		strcpy(temp->next, data); // copy input into temp->element
+
+		// temp points to rear->next (it is always NULL, isn't it?)
+		// we lost the Node300 previously pointed by temp.
+		temp->next = NULL;
+		
+		temp = rear->next;
+
+		// rear points to the same location as temp (NULL)
+		rear = temp;
+
+		// rear->next: dereferencing NULL /!\
+        rear->next = NULL;
+
+		temp = NULL;
+	}
+}
+
+//Dequeue an element from the tail of the list.
+listElement* dequeue(listElement* list) {
+	nodePtr* temp ;
+	nodePtr* front;
+	nodePtr* rear;
+
+	if (rear == NULL && front == NULL)
+	{
+		return NULL;
+	}
+
+	else if (front == rear)
+	{
+		strcpy(temp->next, list);
+		temp = front;
+		delete(temp);
+		temp = NULL;
+		front = NULL;
+		rear = NULL;
+	}
+
+	else
+	{
+		strcpy(temp->next, list);
+		temp = front;
+		front = front->next;
+		temp->next = NULL;
+		delete(temp);
+		temp = NULL;
+	}
 }
